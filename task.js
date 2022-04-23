055e7ee1669f518396e7594a5e290aabcaaa7b07
AlanSkooter <alinasozonovich@gmail.com>
2022-04-23 16:24:16 +0300

    fix convert

--- task.js
+++ task.js
@@ -9,19 +9,15 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 const inputURLS = process.env.INPUT_URLS.split(',');
 const outputDirAudio = process.argv[2];
 
-const convert = (input, output) => {
-    ffmpeg(input)
+(() => {
+    inputURLS.forEach((url) => {
+    const outputAudioPath = path.resolve(outputDirAudio + 'audio' + `${Math.floor(Math.random()*100)}` + '.mp3');
+    ffmpeg(ytdl(url))
         .noVideo()
         .format('mp3')
         .on('end', function() {
             console.log('Processing finished!');
         })
-        .save(output);
-  };
-
-(async () => {
-    await inputURLS.forEach((url) => {
-        const outputAudioPath = path.resolve(outputDirAudio + 'audio' + `${Math.floor(Math.random()*100)}` + '.mp3');
-        convert(ytdl(url), fs.createWriteStream(outputAudioPath));
+        .save(fs.createWriteStream(outputAudioPath));
     });
 })();
